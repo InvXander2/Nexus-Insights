8349e062c58ce1a4d4dd40758b1e4ef3ad3b193a
Xantech007 <113646174+Xantech007@users.noreply.github.com>
2025-05-18 01:00:24 +0200

    page.js

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -8,13 +8,13 @@ const About = () => {
                 <div className="text-white w-1/2 max-[950px]:w-full">
                 <h3 className="text-4xl mb-3 font-semibold bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-cyan-500 ">About Us</h3>
                 <p className="text-[13px] mb-5 leading-loose">
-                    Welcome to CoinBit, your one-stop platform for cryptocurrency investment. We are a team of experienced investors and analysts who are passionate about helping people invest in and profit from the crypto market.
+                    Welcome to Nexus Insights, your one-stop platform for cryptocurrency investment. We are a team of experienced investors and analysts who are passionate about helping people invest in and profit from the crypto market.
 
                     We offer a variety of plans to help you get started in crypto investment,click <Link href="pricing"> <span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-cyan-500 border-b-[1px]">here</span> </Link> to view our pricing plans.<br/>
 
                     We believe that cryptocurrency is a revolutionary technology with the potential to change the world. We also believe that investing in cryptocurrency can be a wise financial decision for those who are willing to do their research and understand the risks involved.<br/>
 
-                    Our goal at CoinBit is to help you make successful crypto investments without prior knowledge of crypto, to avoid loosing your savings to the crypto market which can be quite volatile.
+                    Our goal at Nexus Insights is to help you make successful crypto investments without prior knowledge of crypto, to avoid loosing your savings to the crypto market which can be quite volatile.
                 </p>
                 <h4 className="text-2xl mb-3 bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-cyan-500">Our Mission</h4>
 
@@ -37,7 +37,7 @@ const About = () => {
                 <div className="w-[500px] max-[550px]:w-full">
                     <Link href="contact"> <span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-cyan-500 border-b-[1px]">Contact Us</span> </Link>
                     <p className="text-[13px] mt-3 leading-loose"> If you have any questions, please don't hesitate to contact us. We would be happy to help you get started in crypto investment.
-                    Thank you for visiting CoinBit!</p>
+                    Thank you for visiting Nexus Insights!</p>
                 </div>
             </div>
             <div>
