886dc2f97e7b4d871fab10559db4a02023c0fb4b
Xantech007 <113646174+Xantech007@users.noreply.github.com>
2025-05-18 01:39:54 +0200

    hero.js

--- src/app/components/hero.js
+++ src/app/components/hero.js
@@ -7,7 +7,7 @@ const Hero = () => {
                 <h1 className="text-5xl max-[875px]:text-4xl leading-tight font-semibold">Stay <span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-cyan-500 ">ahead</span> of the financial curve with  <span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-cyan-500">Nexus Insights</span></h1>
                 <p className="text-slate-400 max-w-[450px] max-[720px]:max-w-[600px]">Unlock the power of digital assets and harness the potential of cryptocurrency investments with confidence on our secure and user-friendly platform!</p>
                 <div className="flex gap-5">
-                    <Link href="/signup"><button className="bg-blue-500 p-3 w-40 max-[720px]:w-36 max-[350px]:w-28 max-[350px]:h-10 max-[350px]:text-[13px] rounded-full">Get Started</button></Link>
+                    <Link href="/signup"><button className="bg-blue-500 p-3 w-40 max-[720px]:w-36 max-[350px]:w-28 max-[350px]:h-10 max-[350px]:text-[13px] rounded-full">Register</button></Link>
                     <Link href="/about"><button className=" flex items-center justify-center bg-white text-black p-3 w-40 max-[720px]:w-36 max-[350px]:w-28 max-[350px]:h-10 max-[350px]:w-28 max-[350px]:text-[13px] rounded-full">How it works</button></Link>
                 </div>
                 <div className="flex gap-10 max-[875px]:gap-5 mt-5">
